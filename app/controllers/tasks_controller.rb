2e8ff15a720c9102e01126e7ce51fb76a51db486
shigeo nagashi <shigechikak10@gmail.com>
2019-02-09 12:32:29 +0000

    add status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,16 @@
 class TasksController < ApplicationController
+  before_action :set_task,only: [:show, :edit, :update, :destroy]
+  
+  
   def index
    @tasks = Task.all
   end
   
   def show
-   @task = Task.find(params[:id])
   end
 
  def new
-   @task = Task.new 
+  @task = Task.new
  end
  
  def create
@@ -24,23 +26,19 @@ class TasksController < ApplicationController
  end
 
 def edit
- @task = Task.find(params[:id])
 end
 
 def update
- @task = Task.find(params[:id])
- 
  if @task.update(task_params)
      flash[:success] = 'Taskは正常に更新されました'
      redirect_to @task
  else
-　   flash.now[:danger] = 'Taskは更新されませんでした'
-　   render :edit
+   flash.now[:danger] = 'Taskが更新されませんでした'
+   render :edit
  end
 end
 
  def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'Taskは正常に削除されました'
@@ -49,9 +47,13 @@ end
  end
 
   private
-
+  
+  def set_task
+   @task = Task.find(params[:id])
+  end
+ 
   def task_params
-   params.require(:task).permit(:content)
+   params.require(:task).permit(:content, :status)
   end
   
 end
