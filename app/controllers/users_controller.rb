6671005dcf8627d720ac617c7b048fb14fb100ee
shigeo nagashi <shigechikak10@gmail.com>
2019-03-02 09:18:02 +0000

    login8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,9 @@ class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
   
   def index
-   @users = User.all.page(params[:page])
   end
 
   def show
-   @user = User.find(params[:id])
-   @tasks = @user.tasks.order('created_at DESC').page(params[:page])
   end
 
   def new
